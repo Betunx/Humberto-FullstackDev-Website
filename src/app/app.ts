e9eb0a73b48b248b6931e0f8a6226896cade1df4
Betunx <humbertolpzc.work@gmail.com>
2025-12-03 10:27:27 -0700

    cv section updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   title = 'Portfolio Humberto López';
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.showFooter = !event.url.includes('/resume');
+    });
+  }
 }
 
